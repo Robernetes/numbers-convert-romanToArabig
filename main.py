50f78135e2da20040acdf0e07afdfeedaa60567b
Robernetes <raaguilarm@gmail.com>
2021-04-11 08:10:25 -0500

    On branch main
     Menu principal terminado
    
     Changes to be committed:
    	modified:   main.py

--- main.py
+++ main.py
@@ -2,8 +2,11 @@
 @author : Robernetes
 '''
 
+def to_roman():
+    pass
 
-
+def to_arabig():
+    pass
 
 
 
@@ -11,5 +14,16 @@
 out = False
 while(not out):
     print()
-    print('1. Convertir numeros a Romanos')
-    print('2. Convertir numeros Romanos a ')
+    print('1. Convertir de Arabigo a Romano')
+    print('2. Convertir de Romano a Arabigo')
+    print('3. Salir')
+    opt = int(input('Ingrese una opcion: '))
+    if opt == 1:
+        to_roman()
+    elif opt == 2:
+        to_arabig()
+    elif opt == 3:
+        print('Adios!')
+        out = True
+    else:
+        print('Ingresa una opción que se encuentre en la lista!')
